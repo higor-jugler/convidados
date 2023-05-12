b1cef630e56faba5cc7d45a1081caccd8a2a9d64
Higor Jugler <higorjugler@hotmail.com>
2023-05-12 08:08:55 -0300

    Layout GuestForm

--- app/src/main/java/com/endeavorsheep/convidados/GuestFormActivity.kt
+++ app/src/main/java/com/endeavorsheep/convidados/GuestFormActivity.kt
@@ -2,11 +2,27 @@ package com.endeavorsheep.convidados
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.endeavorsheep.convidados.databinding.ActivityGuestFormBinding
 import com.endeavorsheep.convidados.repository.GuestRepository
 
-class GuestFormActivity : AppCompatActivity() {
+class GuestFormActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityGuestFormBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_guest_form)
+
+        binding = ActivityGuestFormBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonSave.setOnClickListener(this)
+        binding.radioGuestPresent.isChecked = true
+    }
+
+    override fun onClick(view: View) {
+        if (view.id == R.id.button_save) {
+
+        }
     }
 }
