6d87d83949cc7166b65434f00bee849c72c981b3
Higor Jugler <higorjugler@hotmail.com>
2023-05-23 17:07:47 -0300

    Banco do dados Atualizacao

--- app/src/main/java/com/endeavorsheep/convidados/repository/GuestRepository.kt
+++ app/src/main/java/com/endeavorsheep/convidados/repository/GuestRepository.kt
@@ -2,6 +2,7 @@ package com.endeavorsheep.convidados.repository
 
 import android.content.ContentValues
 import android.content.Context
+import com.endeavorsheep.convidados.constants.DataBaseConstants
 import com.endeavorsheep.convidados.repository.model.GuestModel
 
 /**
@@ -51,7 +52,23 @@ class GuestRepository private constructor(context: Context) {
         }
     }
 
-    fun update() {
+    fun update(guest: GuestModel): Boolean {
 
+        return try {
+            val db = guestDataBase.writableDatabase
+            val presence = if (guest.presence) 1 else 0
+
+            val values = ContentValues()
+            values.put(DataBaseConstants.GUEST.COLUMNS.PRESENCE, presence)
+            values.put(DataBaseConstants.GUEST.COLUMNS.NAME, guest.name)
+
+            val selection = DataBaseConstants.GUEST.COLUMNS.ID + " = ?"
+            val args = arrayOf(guest.id.toString())
+
+            db.update(DataBaseConstants.GUEST.TABLE_NAME, values, selection, args)
+            true
+        } catch (e: Exception) {
+            false
+        }
     }
 }
